21283ab9d0051c3073edb7008c06590432875564
Ashiq666 <mn.ashiq07@gmail.com>
2022-12-24 19:46:55 +0600

    Databse , migration, create post model & register

--- iblog/blog/views.py
+++ iblog/blog/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 #dummy post
-posts = [
-    {   'author': 'Nahid',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date': 'December 23, 2022'   
-    },
-    {   'author': 'Ashiq',
-        'title': 'Blog Post 2',
-        'content': 'Second Post content',
-        'date': 'December 24, 2022'   
-    }
-]
 
 # Create your views here.
 def homepage(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
