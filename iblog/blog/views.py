9cc5bfe458d434d3980ca28839f925438353a9a6
Ashiq666 <mn.ashiq07@gmail.com>
2023-01-17 00:04:49 +0600

    create post feature

--- iblog/blog/views.py
+++ iblog/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 #dummy post
 
@@ -11,14 +16,26 @@ def homepage(request):
     }
     return render(request, 'blog/home.html', context)
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-class PostDetailView(DetailView):
+    
+    
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
-
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    #setting new post author(current user)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'}) 
 
